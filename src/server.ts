c80a9462f3157894882ec154df3dc61751e6486a
gustavo-evaristo <gug.henri1@gmail.com>
2021-11-17 13:07:52 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+    const { name, email, password  } = req.body;
+
+    const user = { name, email, password };
+
+    return res.json(user);
+});
+
+app.listen(3333, () => console.log('server runnig on localhost:3333'));
